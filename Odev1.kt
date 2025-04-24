cb98bf314714cd4ddb93de23c072969bc3ebfd99
YusufOck <yusuf.ock2611@gmail.com>
2025-04-24 20:24:17 +0300

    kotlin odev1 puplica
    kotlin odev1 puplica

---
+++ Odev1.kt
@@ -0,0 +1,63 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+    val sehir = "Şanlıurfa"
+    val ulke = "Brezilya"
+    val telefon = "+55 21 99876-5432"
+    val postaKodu = "12345-678"
+    val email = "yusuf.brasil@example.com"
+    val meslek = "Astronom"
+    val stokMiktari = 42
+    val musteriAdi = "Yusuf Demir"
+    val bakiye = 8720.50
+    val dogumGunu = "2001-03-15"
+    val maas = 24500
+    val medeniDurum = "Evli"
+    val urunYorum = "Kalitesi beklentimin çok üstünde."
+
+    val odemeTarihi = "2025-05-01"
+    val odeme = 1349.90
+    val siparisAdeti = 7
+    val arabaModeli = "Tesla Model Y"
+    val kitapAdi = "Gökyüzüne Yolculuk"
+    val yayinlamaTarihi = "2024-02-18"
+    val indirimMiktari = 150.0
+    val odaSayisi = 2
+    val enlem = -8.05
+    val boylam = -34.9
+    val urunAdi = "Akıllı Saat"
+    val yemekFiyati = 79.95
+
+    val marka = "Sony"
+    val muzikAdi = "Sonsuzluk"
+    val videoSuresi = "02:35"
+    val urunPuani = 4.8
+    val resimAdi = "doğa_manzara.jpg"
+    val dosyaFormati = "PNG"
+    val renk = "Lacivert"
+    val renkKodu = "#1A237E"
+    val telefonModeli = "iPhone 15 Pro"
+    val ekranBoyutu = 6.7
+    val agirlik = 189.0
+
+    val ulusalGun = "Cumhuriyet Bayramı"
+    val tatilGunu = "2025-07-20"
+    val rezervasyonTarihi = "2025-06-10"
+    val sokakAdi = "Fırat Caddesi"
+    val otobusHatti = "33B"
+    val kalanDakika = 12
+    val takipKodu = "TRK12345678"
+    val kuponSuresi = "3 gün"
+    val kuponKodu = "YAZ2025"
+    val faturaAdresi = "Harran Mah. No:18, Şanlıurfa"
+
+    println("Müşteri: $musteriAdi, Şehir: $sehir, Ülke: $ulke")
+    println("Telefon: $telefon, Email: $email, Meslek: $meslek")
+    println("Stok: $stokMiktari, Bakiye: $bakiye TL, Maaş: $maas TL")
+    println("Ürün: $urunAdi, Yorum: \"$urunYorum\", Puan: $urunPuani")
+    println("Araba Modeli: $arabaModeli, Kitap: $kitapAdi")
+    println("Video: $muzikAdi ($videoSuresi), Dosya: $resimAdi")
+    println("Ödeme: $odeme TL, İndirim: $indirimMiktari TL")
+    println("Kupon: $kuponKodu, Süresi: $kuponSuresi")
+    println("Fatura Adresi: $faturaAdresi")
+}
